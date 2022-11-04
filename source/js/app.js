353fa8fa50941db7e252c5412e5a28691f857716
Mauro Valdez <josenova0@gmail.com>
2022-11-03 22:34:30 -0300

    header-menu

--- source/js/app.js
+++ source/js/app.js
@@ -4,6 +4,32 @@ import ScrollReveal from "scrollreveal";
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  showMenu()
+})
+
+function showMenu() {
+
+  const navMenu = document.querySelector('#nav-menu')
+  const navToggle = document.querySelector('#nav-toggle')
+  const navClose = document.querySelector('#nav-close')
+  const navLink = document.querySelectorAll('.nav__link')
+
+  if(navToggle) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu')
+    })
+  }
+  if(navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+    })
+  }
+
+  navLink.forEach( n => n.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  }))
+}
 
 
 
