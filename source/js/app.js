2a6a900e9562af3a409bbe6b78371d2d8880baf5
Mauro Valdez <josenova0@gmail.com>
2022-11-04 18:46:53 -0300

    main-header

--- source/js/app.js
+++ source/js/app.js
@@ -1,35 +1,43 @@
-
 import ScrollReveal from "scrollreveal";
 
+window.addEventListener("scroll", () => {
+  scrollHeader();
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+  showMenu();
+});
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  showMenu()
-})
-
+//@note menu
 function showMenu() {
+  const navMenu = document.querySelector("#nav-menu");
+  const navToggle = document.querySelector("#nav-toggle");
+  const navClose = document.querySelector("#nav-close");
+  const navLink = document.querySelectorAll(".nav__link");
 
-  const navMenu = document.querySelector('#nav-menu')
-  const navToggle = document.querySelector('#nav-toggle')
-  const navClose = document.querySelector('#nav-close')
-  const navLink = document.querySelectorAll('.nav__link')
-
-  if(navToggle) {
-    navToggle.addEventListener('click', () => {
-      navMenu.classList.add('show-menu')
-    })
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+    });
   }
-  if(navClose) {
-    navClose.addEventListener('click', () => {
-      navMenu.classList.remove('show-menu')
-    })
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
   }
 
-  navLink.forEach( n => n.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  }))
+  navLink.forEach((n) =>
+    n.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    }),
+  );
 }
 
+//@note scroll background header
+function scrollHeader() {
+  const header = document.querySelector("#header");
 
-
+  window.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+}
