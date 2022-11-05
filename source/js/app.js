f5678eb9f9e18cca5e0890f5d691774001921e9b
Mauro Valdez <josenova0@gmail.com>
2022-11-05 18:39:22 -0300

    scrollup scroll-active-link media-query

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,8 @@ import ScrollReveal from "scrollreveal";
 
 window.addEventListener("scroll", () => {
   scrollHeader();
+  scrollActive();
+  scrollUpShow();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -40,4 +42,34 @@ function scrollHeader() {
   window.scrollY >= 50
     ? header.classList.add("bg-header")
     : header.classList.remove("bg-header");
+}
+
+//@note scroll sections active link
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  const sections = document.querySelectorAll("section[id]");
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      `.nav__menu a[href*=${sectionId}]`,
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+}
+
+//@note show scroll up
+function scrollUpShow() {
+  const scrollUp = document.querySelector("#scroll-up");
+
+  window.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
 }
