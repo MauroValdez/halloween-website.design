a70cbcf5f52bc78999c0cf3d4f29aa6f81714751
Mauro Valdez <josenova0@gmail.com>
2022-11-05 19:13:53 -0300

    media-query animations

--- source/js/app.js
+++ source/js/app.js
@@ -73,3 +73,20 @@ function scrollUpShow() {
     ? scrollUp.classList.add("show-scroll")
     : scrollUp.classList.remove("show-scroll");
 }
+
+//@note scroll reveal animation
+const sr = ScrollReveal( {
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'right'})
